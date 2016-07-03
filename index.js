2b665d14fdaf01a4f80a81261e1de9942ca65a3a
Stefania Delprete <hi@stefaniadelprete.com>
2016-07-03 16:57:22 +0200

    Adding an initial Mindawave grapher

---
+++ index.js
@@ -0,0 +1,58 @@
+var max = 60;
+var thinkgear = require("node-thinkgear-sockets")
+var jf = require('jsonfile');
+
+var client = thinkgear.createClient({ enableRawOutput: false });
+
+var output = [[], [], [], [], [], [], [], []];
+var timeStart = Date.now();
+var count = 0;
+client.on("data", function(data) {
+console.log(data);
+  if (data.eegPower) {
+    // output[0].push({
+    //  date: count,
+    //  value: data.eegPower.delta
+    // });
+    // output[1].push({
+    //  date: count,
+    //  value: data.eegPower.theta
+    // });
+    output[0].push({
+       date: count,
+       value: data.eegPower.lowAlpha
+     });
+    output[1].push({
+       date: count,
+       value: data.eegPower.highAlpha
+     });
+    output[2].push({
+       date: count,
+       value: data.eegPower.lowBeta
+     });
+    output[3].push({
+       date: count,
+       value: data.eegPower.highBeta
+     });
+    //  output[6].push({
+    //    date: count,
+    //    value: data.eegPower.lowGamma
+    //  });
+    //  output[7].push({
+    //    date: count,
+    //    value: data.eegPower.highGamma
+    //  });
+  }
+  ++count;
+    var temp = [];
+    for (var i = 0; i < output.length; i++) {
+      temp[i] = output[i].slice(-max);
+    }
+    jf.writeFile('data/mindwave.json', temp, function(err) {
+      console.log('file written');
+    });
+});
+
+client.connect();
+
+
